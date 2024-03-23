950a0c3472c44595e2098cf1a37c44cb2dd5eadc
HajarTalaat <hager.t.ibrahim@gmail.com>
2024-03-23 17:54:33 +0200

    Overall Changes in One commit message

---
+++ src/login.py
@@ -0,0 +1,11 @@
+# code for facebook login
+print('I can login using my facebook account')
+
+# code for Google login
+print('I can login using my Google account')
+
+# code for Github login
+print('I can login using my Github account')
+
+## Adding Login using LinkedIn
+print('I can login using my LinkedIn account')
