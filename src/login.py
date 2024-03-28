81d21e8e4647e82f32358bae548e2c0db3f01454
HajarTalaat <hager.t.ibrahim@gmail.com>
2024-03-28 15:41:39 +0200

    Implementing login Functionalities

---
+++ src/login.py
@@ -0,0 +1,9 @@
+# code for facebook login
+print('I can login using my facebook account')
+
+# code for Google login
+print('I can login using my Google account')
+
+# code for Github login
+print('I can login using my Github account')
+
